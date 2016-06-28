3c258798e328b7b8ca133f6e4a2d43c7962ed7a6
Susisu <susisu2413@yahoo.co.jp>
2016-06-29 02:19:33 +0900

    Separate file

--- index.js
+++ index.js
@@ -5,45 +5,4 @@
 
 "use strict";
 
-function endModule() {
-    module.exports = Object.freeze({});
-}
-
-class Command {
-    constructor(desc, options, args, action) {
-        this.desc    = desc;
-        this.options = options;
-        this.args    = args;
-        this.action  = action;
-    }
-}
-
-class Argument {
-    constructor(name) {
-        this.name = name;
-    }
-}
-
-class Option {
-    constructor(shortName, longName, arg) {
-        this.shortName = shortName;
-        this.longName  = longName;
-        this.arg       = arg;
-    }
-}
-
-class Subcommand {
-    constructor(name, cmd) {
-        this.name = name;
-        this.cmd  = cmd;
-    }
-}
-
-class Group {
-    constructor(desc, subcmds) {
-        this.desc    = desc;
-        this.subcmds = subcmds;
-    }
-}
-
-endModule();
+module.exports = require("./lib/optics.js");
