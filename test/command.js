b7c9a2b1b09bec4c15d4dc33e84c38ef54afca59
Susisu <susisu2413@yahoo.co.jp>
2016-07-04 22:17:08 +0900

    Add tests for CommandBase

--- test/command.js
+++ test/command.js
@@ -5,5 +5,30 @@
 
 "use strict";
 
+const chai   = require("chai");
+const expect = chai.expect;
+
+const command = require("../lib/command.js");
+const output  = require("../lib/output.js");
+
 describe("command", () => {
+    describe("CommandBase", () => {
+        describe("constructor()", () => {
+            it("should create a new CommandBase instance", () => {
+                let cmd = new command.CommandBase();
+                expect(cmd).to.be.an.instanceOf(command.CommandBase);
+            });
+        });
+
+        describe("#run(out, argv)", () => {
+            it("should throw an Error", () => {
+                let out = new output.Output(
+                    _ => { throw new Error("unexpected output"); },
+                    _ => { throw new Error("unexpected output"); }
+                );
+                let cmd = new command.CommandBase();
+                expect(() => { cmd.run(out, []); }).to.throw(Error);
+            });
+        });
+    });
 });
