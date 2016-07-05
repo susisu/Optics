5f9d9b6d67b5a5581a64f9ffd0ba5b87f36141f5
Susisu <susisu2413@yahoo.co.jp>
2016-07-05 20:15:27 +0900

    Add pending tests

--- test/command.js
+++ test/command.js
@@ -31,4 +31,21 @@ describe("command", () => {
             });
         });
     });
+
+    describe("Command", () => {
+        describe("constructor(desc, args, opts, action)", () => {
+            it("should create a new Command instance");
+            it("should throw an error if 'args' is not an array of Argument");
+            it("should throw an error if 'opts' is not an array of Option");
+            it("should throw an error if 'action' is not a function");
+            it("should throw an error if a required argument occurs after an optional argument");
+        });
+
+        describe("#run(out, argv)", () => {
+            it("should parse 'argv' and call the action with arguments and options if arguments are correct");
+            it("should throw an error if 'out' is not an instance of Output");
+            it("should throw an error if 'argv' is not an array");
+            it("should write error message to 'out.err' if arguments are incorrect");
+        });
+    });
 });
