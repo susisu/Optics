483a54425f0a622b95a38938f7c70421e9a370d5
Susisu <susisu2413@yahoo.co.jp>
2016-07-30 19:45:48 +0900

    Add pending tests for the subcommand module

--- test/subcommand.js
+++ test/subcommand.js
@@ -6,4 +6,20 @@
 "use strict";
 
 describe("subcommand", () => {
+    describe("Subcommand", () => {
+        describe("constructor(name, cmd)", () => {
+            it("should create a new Subcommand instance");
+            it("should throw a TypeError if 'name' is not a string");
+            it("should throw a TypeError if 'cmd' is not an instance of CommandBase");
+        });
+    });
+
+    describe("CommandGroup", () => {
+        describe("constructor(desc, subcmds, defaultCmd)", () => {
+            it("should create a new CommandGroup instance");
+            it("should throw a TypeError if 'desc' is not a string");
+            it("should throw a TypeError if 'subcmds' is not an array of Subcommand");
+            it("should throw a TypeError if 'defaultCmd' is not an instance of CommandBase nor null");
+        });
+    });
 });
