0a7c5f72ec360e4f0fa1417c360224f24260ecb0
Susisu <susisu2413@yahoo.co.jp>
2016-08-01 12:51:19 +0900

    Add tests for the Subcommand constructor

--- test/subcommand.js
+++ test/subcommand.js
@@ -5,12 +5,70 @@
 
 "use strict";
 
+const chai   = require("chai");
+const expect = chai.expect;
+
+const command    = require("../lib/command.js");
+const subcommand = require("../lib/subcommand.js");
+
 describe("subcommand", () => {
     describe("Subcommand", () => {
         describe("constructor(name, cmd)", () => {
-            it("should create a new Subcommand instance");
-            it("should throw a TypeError if 'name' is not a string");
-            it("should throw a TypeError if 'cmd' is not an instance of CommandBase");
+            it("should create a new Subcommand instance", () => {
+                let cmd = new command.Command(
+                    "test command",
+                    [],
+                    [],
+                    (args, opts) => {}
+                );
+                let subcmd = new subcommand.Subcommand("test", cmd);
+                expect(subcmd).to.be.an.instanceOf(subcommand.Subcommand);
+            });
+
+            it("should throw a TypeError if 'name' is not a string", () => {
+                let cmd = new command.Command(
+                    "test command",
+                    [],
+                    [],
+                    (args, opts) => {}
+                );
+                function construct(name) {
+                    return () => {
+                        new subcommand.Subcommand(name, cmd);
+                    };
+                }
+                expect(construct("test")).not.to.throw(TypeError);
+
+                expect(construct(null)).to.throw(TypeError);
+                expect(construct(undefined)).to.throw(TypeError);
+                expect(construct(3.14)).to.throw(TypeError);
+                expect(construct(true)).to.throw(TypeError);
+                expect(construct({})).to.throw(TypeError);
+                expect(construct(() => {})).to.throw(TypeError);
+            });
+
+            it("should throw a TypeError if 'cmd' is not an instance of CommandBase", () => {
+                let cmd = new command.Command(
+                    "test command",
+                    [],
+                    [],
+                    (args, opts) => {}
+                );
+                function construct(cmd) {
+                    return () => {
+                        new subcommand.Subcommand("test", cmd);
+                    };
+                }
+                expect(construct(cmd)).not.to.throw(TypeError);
+
+                expect(construct(null)).to.throw(TypeError);
+                expect(construct(undefined)).to.throw(TypeError);
+                expect(construct("foobar")).to.throw(TypeError);
+                expect(construct(3.14)).to.throw(TypeError);
+                expect(construct(true)).to.throw(TypeError);
+                expect(construct({})).to.throw(TypeError);
+                expect(construct(() => {})).to.throw(TypeError);
+            });
         });
     });
 
